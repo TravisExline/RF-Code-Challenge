d54f3253664ddcbf5832f2975cb5d55763811c77
TravisExline <t.exline22@gmail.com>
2020-10-27 08:28:52 -0500

    profile section framing

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,27 @@
 import React from 'react' 
+import '../style/Profile.css'
 
 class Profile extends React.Component {
     render() {
         return(
-            <div>
-                <h1>I am a profile</h1>
+            <div className="profile-holder">
+                <div>
+                    <h1 className="profile-name">Filip Martin Jose</h1>
+                    <p className="profile-location">Los Angeles</p>
+                </div>
+                <button className='pro-btn'>Pro Level</button>
+                <div className='follower-holder'>
+                    <p className="followers-head">Followers</p>
+                    <h2 className="follower-count">980</h2>
+                </div>
+                <div className="project-holder">
+                    <p className="projects-head">Projects</p>
+                    <h2 className="projects-count">142</h2>
+                </div>
+                <div className='rank-holder'>
+                    <p className="rank-head">Rank</p>
+                    <h2 className="rank-count">129</h2>
+                </div>
             </div>
         )
     }
